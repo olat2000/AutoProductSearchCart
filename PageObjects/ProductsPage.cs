3da284c0383ed5c0e0e67dcfd433e6402900f539
olat2000 <olatunji.bamgbose2010@mail.com>
2023-09-20 17:57:09 +0100

    Code added

--- PageObjects/ProductsPage.cs
+++ PageObjects/ProductsPage.cs
@@ -14,15 +14,15 @@ namespace AutoProductSearchCart.PageObjects
             readJsonData = container.Resolve<ReadJsonData>();
         }
 
-        IWebElement loginSignBttn => Driver.FindthisElement(By.XPath("//*[@href='/login']"));
-        IWebElement userEmail => Driver.FindthisElement(By.CssSelector("*[data-qa='login-email'"));
-        IWebElement userPassword => Driver.FindthisElement(By.CssSelector("*[data-qa='login-password'"));
-        IWebElement loginBttn => Driver.FindthisElement(By.XPath("(//*[@type='submit'])[1]"));
-        IWebElement productBttn => Driver.FindthisElement(By.XPath("//*[@href='/products']"));
-        IWebElement searchBar => Driver.FindthisElement(By.Id("search_product"));
-        IWebElement search => Driver.FindthisElement(By.CssSelector("[id='submit_search']"));
-        IWebElement addToCart => Driver.FindthisElement(By.XPath("(//a[@data-product-id='33'])[1]"));
-        IWebElement viewCart => Driver.FindthisElement(By.XPath("(//*[@href='/view_cart'])[2]"));
+        IWebElement loginSignBttn => Driver!.FindthisElement(By.XPath("//*[@href='/login']"));
+        IWebElement userEmail => Driver!.FindthisElement(By.CssSelector("*[data-qa='login-email'"));
+        IWebElement userPassword => Driver!.FindthisElement(By.CssSelector("*[data-qa='login-password'"));
+        IWebElement loginBttn => Driver!.FindthisElement(By.XPath("(//*[@type='submit'])[1]"));
+        IWebElement productBttn => Driver!.FindthisElement(By.XPath("//*[@href='/products']"));
+        IWebElement searchBar => Driver!.FindthisElement(By.Id("search_product"));
+        IWebElement search => Driver!.FindthisElement(By.CssSelector("[id='submit_search']"));
+        IWebElement addToCart => Driver!.FindthisElement(By.XPath("(//a[@data-product-id='33'])[1]"));
+        IWebElement viewCart => Driver!.FindthisElement(By.XPath("(//*[@href='/view_cart'])[2]"));
 
         public void NavigateToUrl() => 
             Driver?.Navigate().GoToUrl(readJsonData.GetData("environ:url"));
@@ -50,7 +50,7 @@ namespace AutoProductSearchCart.PageObjects
             catch (Exception){ throw;}
         }
 
-        public bool ValidateProductsPage() => Driver.Url.Equals("https://www.automationexercise.com/products");
+        public bool ValidateProductsPage() => Driver!.Url.Equals("https://www.automationexercise.com/products");
 
         public void InputProductToSearch() => 
             searchBar.InputTextToElement(readJsonData.GetData("environ:searchword"));
@@ -58,14 +58,14 @@ namespace AutoProductSearchCart.PageObjects
         public void ClickSearch() => search.ClicktheElement();
         
         public bool ValidateReelatedProductsAreVisible() =>
-             Driver.Url.Equals("https://www.automationexercise.com/products?search=Jeans");
+             Driver!.Url.Equals("https://www.automationexercise.com/products?search=Jeans");
 
         public void ClickAddToCart() => addToCart.ClicktheElement();
 
         public void ClickViewCart() => viewCart.ClicktheElement();
 
         public bool ProductShouldBeVisibled() =>
-            Driver.Url.Equals("https://www.automationexercise.com/view_cart");
+            Driver!.Url.Equals("https://www.automationexercise.com/view_cart");
     }
  
 }
